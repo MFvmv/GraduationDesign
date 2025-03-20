9b0a2b3a0b6cd9c0858e5dd6ce7238612ad8630f
MFvmv <41670034+MFvmv@users.noreply.github.com>
2025-03-20 13:35:21 +0800

    Update AdminApplication.java

--- hospital/admin/src/main/java/com/hospital/admin/AdminApplication.java
+++ hospital/admin/src/main/java/com/hospital/admin/AdminApplication.java
@@ -7,8 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import com.hospital.common.config.CustomFilter;
 
-@SpringBootApplication
-@MapperScan("com.hospital.admin.dao")
+@SpringBootApplication(scanBasePackages = {"com.hospital.admin.config"})
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
